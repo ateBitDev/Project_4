c62c4022e5901640ad10f67951258859078201e0
ateBitDev <raul.santiago.aguirre@gmail.com>
2019-01-18 21:16:27 -0800

    quick push

--- js/app.js
+++ js/app.js
@@ -5,7 +5,20 @@ startButton.addEventListener('click', () =>  game.startGame())
 //gets a referance to all the buttons and elements needed to create a end game message
 const buttons = document.querySelectorAll('BUTTON');
 const mess = document.createElement('h2')
-const overlay = document.getElementById('overlay');
+// const h1 = document.querySelector('h1')
+// const overlay = document.getElementById('overlay');
+//
+// //const newDiv = document.createElement('div');
+// const pic = document.createElement('li');
+//
+// pic.innerHTML = '<img src="images/flake.jpg">';
+// pic.append(h1)
+// overlay.append(pic)
+//
+// document.querySelector('h1').style.color = 'black'
+
+
+
 overlay.append(mess);
 //when a button is pushed it is passed to handleInteraction
 buttons.forEach(button => button.addEventListener('click', (e) =>
